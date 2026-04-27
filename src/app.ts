e7d1374220dd388d81ed40ae6c07c9de71d0fec4
MISFOfficial <muksitul44@gmail.com>
2026-04-28 02:54:57 +0600

    feat: install cors and configure middleware for JSON and URL-encoded request parsing

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
 const app: Application = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// testing
+app.get("/", (req: Request, res: Response) => {
+  res.send("University Management Backend is running");
 });
 
 export default app;
- 
